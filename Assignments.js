334cec0259163df03679491e5670c363c8dbc64b
Michael-10-Ai <omatolamichael@gmail.com>
2026-06-25 06:57:00 +0100

    Add files via upload

--- Assignments.js
+++ Assignments.js
@@ -274,3 +274,22 @@ function camelize(lalae) {
     return lalae.toUpperCase()
 }
 console.log(camelize('JavaScriptExercises'))
+
+
+
+// .Exract Characters
+
+// Write a JavaScript function to extract a specified number of characters from a string.
+
+// Test Data :
+
+// console.log(truncate_string("Robin Singh",4));
+// "Robi"
+
+function truncate_string(lolo){
+    let nolo = lolo.substring(4);
+    return nolo
+}
+console.log(truncate_string("Robin Singh",4));
+"Robi"
+
